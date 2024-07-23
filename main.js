e3bc6631c86362da5693a2357020660531fb8e89
viktorKorneev <korneevviktor90@yandex.ru>
2024-07-23 19:46:45 +0300

    step-7

--- main.js
+++ main.js
@@ -7,30 +7,46 @@ while (!isWinner) {
   userChoice = userChoice.toLowerCase();
 
   let randomNum = Math.floor(Math.random() * 3);
-
-  if (randomNum === 0) {
-    computerChoice = "камень";
-  } else if (randomNum === 1) {
-    computerChoice = "ножницы";
-  } else {
-    computerChoice = "бумага";
+  switch (randomNum) {
+    case 0:
+      computerChoice = "камень";
+      break;
+    case 1:
+      computerChoice = "ножницы";
+      break;
+    case 2:
+      computerChoice = "бумага";
+      break;
   }
 
+  //   if (randomNum === 0) {
+  //     computerChoice = "камень";
+  //   } else if (randomNum === 1) {
+  //     computerChoice = "ножницы";
+  //   } else {
+  //     computerChoice = "бумага";
+  //   }
+
   if (
-    userChoice === "камень" || userChoice === "ножницы" || userChoice === "бумага") {
-  
-        if(userChoice === computerChoice) {
-            alert("Ничья, играем ещё раз!")
-        } else if ((userChoice === "камень" && computerChoice === "ножницы") || 
-                    (userChoice === "ножницы" && computerChoice === "бумага") || 
-                    (userChoice === "бумага" && computerChoice === "камень")) {
-        alert("Ты выиграл!")
-        isWinner = true
-        } else {
-            alert("Ты проиграл!")
-            isWinner = true
-        }
-  
+    userChoice === "камень" ||
+    userChoice === "ножницы" ||
+    userChoice === "бумага"
+  ) {
+    if (userChoice === computerChoice) {
+      alert("Ничья, играем ещё раз!");
+    } else  {
+        const isUserWinner =
+        (userChoice === "камень" && computerChoice === "ножницы") ||
+        (userChoice === "ножницы" && computerChoice === "бумага") ||
+        (userChoice === "бумага" && computerChoice === "камень")
+
+        const message = isUserWinner ? "Ты выиграл!" : "Ты проиграл!"
+        alert(message)
+       
+        isWinner = true;
+      
+    }
+      
   } else {
     alert("Пожалуйста, введи коррктный ход: камень, ножницы или бумага.");
   }
