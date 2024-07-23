a2f79047665206ab277de27fd1c4cf3607958ad3
viktorKorneev <korneevviktor90@yandex.ru>
2024-07-23 19:03:54 +0300

    step-4

--- main.js
+++ main.js
@@ -5,4 +5,14 @@ let isWinner = false
 while (!isWinner ) {
     userChoice = prompt("Выберите камень, ножницы или бумага:")
     userChoice = userChoice.toLowerCase()
+
+    let randomNum = Math.floor(Math.random() * 3)
+
+    if (randomNum === 0) {
+        computerChoice = "камень"
+    } else if (randomNum === 1) {
+        computerChoice = "ножницы"
+    } else {
+        computerChoice = "бумага"
+    }
 }
